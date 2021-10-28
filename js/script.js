eb6c3e95dd4d66373a0bdba7c5e660e92f86eb46
Antonio Sousa <antoniowsj@outlook.com>
2021-10-28 14:31:30 -0300

    Add choose and change player

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 var player = null;
+var playerSelected = document.getElementById('player-selected');
+
+changePlayer('X');
 
 function chooseSquare(id) {
-  console.log(id);
+  var square = document.getElementById(id);
+  square.innerHTML = player;
+  square.style.color = '#000000'
+}
+
+function changePlayer(valor) {
+  player = valor;
+  playerSelected.innerHTML = player;
+
 }
